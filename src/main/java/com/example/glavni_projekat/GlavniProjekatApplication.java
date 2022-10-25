09f8f1064acac2b7136d7710e5d1f9b5e9704049
Vlada <vlap986@gmail.com>
2022-10-25 20:52:19 +0200

    First commit

---
+++ src/main/java/com/example/glavni_projekat/GlavniProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.example.glavni_projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlavniProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlavniProjekatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/glavni_projekat/GlavniProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glavni_projekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlavniProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
